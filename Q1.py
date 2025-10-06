b1a0a2ee4134b6596dfb3eab459abe048fea1958
Pavan060703 <134184250+Pavan060703@users.noreply.github.com>
2025-10-06 22:47:21 +0530

    Add files via upload

---
+++ Q1.py
@@ -0,0 +1,8 @@
+m1=int(input("Enter the marks of first subject"))
+m2=int(input("Enter the marks of second subject"))
+m3=int(input("Enter the marks of third subject"))
+m4=int(input("Enter the marks of fourth subject"))
+m5=int(input("Enter the marks of fifth subject"))
+total=m1+m2+m3+m4+m5
+per=total/5
+print("Percentage is ",per)
---
+++ Q2.py
@@ -0,0 +1,4 @@
+length=float(input("Enter the length of rectangle"))
+breadth=float(input("Enter the breadth of rectangle"))
+area_of_rectangle=length*breadth
+print("Area of rectangle is:",area_of_rectangle)
