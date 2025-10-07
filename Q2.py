66c3493cadb851d67111199b96d13e3771932b69
Pavan060703 <134184250+Pavan060703@users.noreply.github.com>
2025-10-07 11:35:27 +0530

    Delete Q2.py

--- Q2.py
+++
@@ -1,4 +0,0 @@
-length=float(input("Enter the length of rectangle"))
-breadth=float(input("Enter the breadth of rectangle"))
-area_of_rectangle=length*breadth
-print("Area of rectangle is:",area_of_rectangle)
