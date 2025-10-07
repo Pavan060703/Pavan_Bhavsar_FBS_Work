f818dc2a7c97edc6028a29a47b8ddc03e811438d
Pavan060703 <134184250+Pavan060703@users.noreply.github.com>
2025-10-07 11:34:23 +0530

    Add files via upload

---
+++ FBS/Core Python/Assignment 1/Q10.py
@@ -0,0 +1,5 @@
+side=float(input("Enter the side of equilateral triangle"))
+
+area=(((3**0.5)/4)*(side**2))
+
+print("Area of triangle",area)
---
+++ FBS/Core Python/Assignment 1/Q4.py
@@ -0,0 +1,6 @@
+#Simple Interest
+P=float(input("Enter the principal amount"))
+T=float(input("Enter the years"))
+R=float(input("Enter the rate of interest"))
+Simple_Interest=(P*T*R)/100
+print("Simple_Interest is ",Simple_Interest)
