d1c83485c0fd35784fbd8866c7f4928b1845d078
Pavan060703 <134184250+Pavan060703@users.noreply.github.com>
2026-01-07 07:26:43 +0530

    Add files via upload

---
+++ D:\FBS\Core Python\Assignments/Assignment 2/Q6.py
@@ -0,0 +1,9 @@
+basic_salary=float(input("Enter the basic salary of employee"))
+
+da=(10/100)*basic_salary
+ta=(12/100)*basic_salary
+hra=(15/100)*basic_salary
+
+total_salary=basic_salary+da+ta+hra
+
+print("Total salary of Employee",total_salary)
