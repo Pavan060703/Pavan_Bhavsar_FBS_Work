f461c9122b6f151a3e49148a85b20fc47fa92e23
Pavan060703 <134184250+Pavan060703@users.noreply.github.com>
2026-01-07 07:12:33 +0530

    Add files via upload

---
+++ Assignment 1/Q3.py
@@ -0,0 +1,6 @@
+num1=int(input("Enter the first number"))
+num2=int(input("Enter the second number"))
+remainder=num1%num2
+quotient=num1//num2
+print("Quotient is ",quotient)
+print("Remainder is ",remainder)
---
+++ Assignment 1/Q6.py
@@ -0,0 +1,4 @@
+First_angle=float(input("Enter the first angle of triangle"))
+Second_angle=float(input("Enter the second angle of triangle"))
+third_angle=180-First_angle-Second_angle
+print("Third angle is ",third_angle)
